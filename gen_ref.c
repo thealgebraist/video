233e54be7ea3fd4e15edb4e950af306d53c5ba61
dev <dev@dev.com>
2026-01-25 19:09:57 +0100

    Align reference and formal generators with dynamic scene durations based on voiceover length

--- gen_ref.c
+++ gen_ref.c
@@ -10,7 +10,12 @@
 #define WIDTH 1280
 #define HEIGHT 720
 #define FPS 25
-#define DURATION 10
+#define NUM_SCENES 10
+
+static const char *image_names[NUM_SCENES] = {
+    "01_security_line", "02_boot_struggle", "03_sad_sandwich", "04_delayed_sign", "05_gate_lice",
+    "06_tiny_seat", "07_crying_baby", "08_seat_recline", "09_turbulence", "10_lost_suitcase"
+};
 
 int main() {
     AVFormatContext *oc = NULL;
@@ -44,58 +49,108 @@ int main() {
     avcodec_parameters_from_context(ast->codecpar, actx);
 
     avio_open(&oc->pb, "ref.mpg", AVIO_FLAG_WRITE);
-    avformat_write_header(oc, NULL);
+    if (avformat_write_header(oc, NULL) < 0) return 1;
 
-    // Load Image using FFmpeg pipe/format
-    AVFormatContext *ic = NULL;
-    avformat_open_input(&ic, "../aigen/airport/assets_airport/images/01_security_line.png", NULL, NULL);
-    avformat_find_stream_info(ic, NULL);
-    const AVCodec *icodec = avcodec_find_decoder(ic->streams[0]->codecpar->codec_id);
-    AVCodecContext *ictx = avcodec_alloc_context3(icodec);
-    avcodec_parameters_to_context(ictx, ic->streams[0]->codecpar);
-    avcodec_open2(ictx, icodec, NULL);
-
-    AVFrame *iframe = av_frame_alloc();
-    AVPacket *ipkt = av_packet_alloc();
-    av_read_frame(ic, ipkt);
-    avcodec_send_packet(ictx, ipkt);
-    avcodec_receive_frame(ictx, iframe);
-
-    // Convert to YUV420P
-    struct SwsContext *sws = sws_getContext(ictx->width, ictx->height, ictx->pix_fmt, WIDTH, HEIGHT, AV_PIX_FMT_YUV420P, SWS_BICUBIC, NULL, NULL, NULL);
     AVFrame *vframe = av_frame_alloc();
     vframe->format = vctx->pix_fmt; vframe->width = WIDTH; vframe->height = HEIGHT;
     av_frame_get_buffer(vframe, 0);
-    sws_scale(sws, (const uint8_t * const *)iframe->data, iframe->linesize, 0, ictx->height, vframe->data, vframe->linesize);
 
-    AVFrame *aframe = av_frame_alloc();
-    aframe->format = actx->sample_fmt; aframe->sample_rate = actx->sample_rate;
-    av_channel_layout_copy(&aframe->ch_layout, &actx->ch_layout);
-    aframe->nb_samples = actx->frame_size;
-    av_frame_get_buffer(aframe, 0);
-    for (int ch = 0; ch < actx->ch_layout.nb_channels; ch++) memset(aframe->data[ch], 0, aframe->nb_samples * sizeof(float));
+    AVFrame *a_out_frame = av_frame_alloc();
+    a_out_frame->format = actx->sample_fmt; a_out_frame->sample_rate = actx->sample_rate;
+    av_channel_layout_copy(&a_out_frame->ch_layout, &actx->ch_layout);
+    a_out_frame->nb_samples = actx->frame_size;
+    av_frame_get_buffer(a_out_frame, 0);
 
     AVPacket *pkt = av_packet_alloc();
-    int64_t v_pts = 0, a_pts = 0;
-    while (v_pts < DURATION * FPS) {
-        vframe->pts = v_pts++;
-        avcodec_send_frame(vctx, vframe);
-        while (avcodec_receive_packet(vctx, pkt) == 0) {
-            av_packet_rescale_ts(pkt, vctx->time_base, vst->time_base);
-            pkt->stream_index = vst->index;
-            av_interleaved_write_frame(oc, pkt);
-            av_packet_unref(pkt);
-        }
-        while (av_rescale_q(a_pts, actx->time_base, vctx->time_base) < v_pts) {
-            aframe->pts = a_pts; a_pts += aframe->nb_samples;
-            avcodec_send_frame(actx, aframe);
-            while (avcodec_receive_packet(actx, pkt) == 0) {
-                av_packet_rescale_ts(pkt, actx->time_base, ast->time_base);
-                pkt->stream_index = ast->index;
+    int64_t v_pts_offset = 0;
+    int64_t a_pts = 0;
+
+    for (int s = 0; s < NUM_SCENES; s++) {
+        AVFormatContext *ac = NULL;
+        char vo_path[1024];
+        snprintf(vo_path, sizeof(vo_path), "../aigen/airport/assets_airport/voice/vo_%03d.wav", s);
+        if (avformat_open_input(&ac, vo_path, NULL, NULL) < 0) continue;
+        avformat_find_stream_info(ac, NULL);
+        
+        double duration = (double)ac->duration / AV_TIME_BASE;
+        int num_frames = (int)(duration * FPS);
+        if (num_frames == 0) num_frames = 1;
+        printf("Scene %d (%s): Duration %.2fs, %d frames\n", s, image_names[s], duration, num_frames);
+
+        const AVCodec *a_in_codec = avcodec_find_decoder(ac->streams[0]->codecpar->codec_id);
+        AVCodecContext *a_in_ctx = avcodec_alloc_context3(a_in_codec);
+        avcodec_parameters_to_context(a_in_ctx, ac->streams[0]->codecpar);
+        avcodec_open2(a_in_ctx, a_in_codec, NULL);
+        
+        SwrContext *swr = NULL;
+        swr_alloc_set_opts2(&swr, &actx->ch_layout, actx->sample_fmt, actx->sample_rate, &a_in_ctx->ch_layout, a_in_ctx->sample_fmt, a_in_ctx->sample_rate, 0, NULL);
+        swr_init(swr);
+        
+        AVFrame *a_in_frame = av_frame_alloc();
+        AVPacket *ipkt = av_packet_alloc();
+
+        AVFormatContext *ic = NULL;
+        char img_path[1024];
+        snprintf(img_path, sizeof(img_path), "../aigen/airport/assets_airport/images/%s.png", image_names[s]);
+        if (avformat_open_input(&ic, img_path, NULL, NULL) < 0) goto cleanup_audio;
+        avformat_find_stream_info(ic, NULL);
+        const AVCodec *icodec = avcodec_find_decoder(ic->streams[0]->codecpar->codec_id);
+        AVCodecContext *ictx = avcodec_alloc_context3(icodec);
+        avcodec_parameters_to_context(ictx, ic->streams[0]->codecpar);
+        avcodec_open2(ictx, icodec, NULL);
+        AVFrame *iframe = av_frame_alloc();
+        AVPacket *img_pkt = av_packet_alloc();
+        av_read_frame(ic, img_pkt);
+        avcodec_send_packet(ictx, img_pkt);
+        avcodec_receive_frame(ictx, iframe);
+        struct SwsContext *sws = sws_getContext(ictx->width, ictx->height, ictx->pix_fmt, WIDTH, HEIGHT, AV_PIX_FMT_YUV420P, SWS_BICUBIC, NULL, NULL, NULL);
+        sws_scale(sws, (const uint8_t * const *)iframe->data, iframe->linesize, 0, ictx->height, vframe->data, vframe->linesize);
+
+        for (int f = 0; f < num_frames; f++) {
+            vframe->pts = v_pts_offset + f;
+            avcodec_send_frame(vctx, vframe);
+            while (avcodec_receive_packet(vctx, pkt) == 0) {
+                av_packet_rescale_ts(pkt, vctx->time_base, vst->time_base);
+                pkt->stream_index = vst->index;
                 av_interleaved_write_frame(oc, pkt);
                 av_packet_unref(pkt);
             }
+
+            while (av_rescale_q(a_pts, (AVRational){1, actx->sample_rate}, vctx->time_base) < vframe->pts + 1) {
+                if (av_read_frame(ac, ipkt) >= 0) {
+                    avcodec_send_packet(a_in_ctx, ipkt);
+                    if (avcodec_receive_frame(a_in_ctx, a_in_frame) >= 0) {
+                        swr_convert(swr, a_out_frame->data, actx->frame_size, (const uint8_t **)a_in_frame->data, a_in_frame->nb_samples);
+                        a_out_frame->pts = a_pts; a_pts += actx->frame_size;
+                        avcodec_send_frame(actx, a_out_frame);
+                        while (avcodec_receive_packet(actx, pkt) == 0) {
+                            av_packet_rescale_ts(pkt, actx->time_base, ast->time_base);
+                            pkt->stream_index = ast->index;
+                            av_interleaved_write_frame(oc, pkt);
+                            av_packet_unref(pkt);
+                        }
+                    }
+                    av_packet_unref(ipkt);
+                } else {
+                    for (int ch = 0; ch < actx->ch_layout.nb_channels; ch++) memset(a_out_frame->data[ch], 0, actx->frame_size * sizeof(float));
+                    a_out_frame->pts = a_pts; a_pts += actx->frame_size;
+                    avcodec_send_frame(actx, a_out_frame);
+                    while (avcodec_receive_packet(actx, pkt) == 0) {
+                        av_packet_rescale_ts(pkt, actx->time_base, ast->time_base);
+                        pkt->stream_index = ast->index;
+                        av_interleaved_write_frame(oc, pkt);
+                        av_packet_unref(pkt);
+                    }
+                }
+            }
         }
+        v_pts_offset += num_frames;
+
+        sws_freeContext(sws); av_frame_free(&iframe); av_packet_free(&img_pkt);
+        avcodec_free_context(&ictx); avformat_close_input(&ic);
+cleanup_audio:
+        swr_free(&swr); av_frame_free(&a_in_frame); av_packet_free(&ipkt);
+        avcodec_free_context(&a_in_ctx); avformat_close_input(&ac);
     }
 
     av_write_trailer(oc);
--- save_mpeg2.py
+++ save_mpeg2.py
@@ -3,97 +3,78 @@ import subprocess
 import numpy as np
 import scipy.io.wavfile as wavfile
 
-# Header bytes generated from verified Coq mpeg2_spec model:
-SEQ_HDR = bytes([0, 0, 1, 179, 80, 2, 208, 51, 255, 255, 226, 40])
-SEQ_EXT = bytes([0, 0, 1, 181, 20, 168, 0, 1, 0, 0])
-GOP_HDR = bytes([0, 0, 1, 184, 0, 8, 0, 0])
-PIC_HDR = bytes([0, 0, 1, 0, 0, 8, 255, 248])
-PIC_EXT = bytes([0, 0, 1, 181, 143, 255, 243, 65, 128])
-SEQ_END = bytes([0, 0, 1, 183])
+# --- 1-1 Synchronized bytes from Coq libav_mpeg2_model ---
+# Covers: Seq -> SeqExt -> Pic -> PicExt -> Slice -> Block
+FORMAL_CORE_BLOCK = bytes([0, 0, 1, 179, 80, 2, 208, 51, 255, 255, 226, 40, 0, 0, 1, 181, 20, 170, 0, 1, 0, 0, 0, 0, 1, 0, 0, 15, 255, 248, 0, 0, 1, 181, 143, 255, 255, 64, 0, 0, 0, 1, 161, 64])
 
-PES_COMMON_HDR = bytes([128, 128, 5]) 
-LPCM_HDR = bytes([160, 1, 0, 0, 1])
+LPCM_HDR = bytes([160, 1, 0, 0, 1]) # 48k/16b/Stereo
 
 def encode_pts(pts):
     return bytes([
         (33 + (pts // 1073741824) * 2) & 0xff,
         (pts // 4194304) % 256,
-        (((pts // 16384) % 128) * 2 + 1) & 0xff,
-        (pts // 64) % 256,
-        ((pts % 64) * 2 + 1) & 0xff
+        ((pts // 32768) % 128 * 2 + 1) & 0xff,
+        (pts // 128) % 256,
+        (pts % 128 * 2 + 1) & 0xff
     ])
 
-def write_pack_header(f, pts):
-    f.write(bytes([0, 0, 1, 186]))
-    scr = pts 
-    f.write(bytes([0x44 | ((scr >> 30) & 7) << 1, (scr >> 22) & 0xff, ((scr >> 15) & 0x7f) << 1 | 1, (scr >> 7) & 0xff, (scr & 0x7f) << 1 | 1, 0x01]))
-    f.write(bytes([0x01, 0x00])) 
+def get_formal_pack_bytes(pts):
+    scr = pts
+    return bytes([
+        0, 0, 1, 186,
+        (64 + (scr // 1073741824) * 4 + 4 + (scr // 134217728) % 4) & 0xff,
+        (scr // 524288) % 256,
+        ((scr // 2048) % 128 * 4 + 4 + (scr // 512) % 4) & 0xff,
+        (scr // 2) % 256,
+        (scr % 2 * 128 + 1) & 0xff,
+        1, 0, 1, 129, 0 
+    ])
 
 def write_pes(f, sid, payload, pts, is_lpcm=False):
-    write_pack_header(f, pts)
+    f.write(get_formal_pack_bytes(pts))
     extra_len = len(LPCM_HDR) if is_lpcm else 0
     plen = len(payload) + 3 + 5 + extra_len
-    f.write(bytes([0, 0, 1, sid]))
-    f.write(bytes([plen >> 8, plen & 0xff]))
-    f.write(PES_COMMON_HDR)
+    f.write(bytes([0, 0, 1, sid, plen >> 8, plen & 0xff, 0x80, 0x80, 0x05]))
     f.write(encode_pts(pts))
     if is_lpcm: f.write(LPCM_HDR)
     f.write(payload)
 
 def get_audio_data_and_duration(vo_path):
-    if not os.path.exists(vo_path):
-        return np.zeros((48000, 2), dtype=np.int16), 1.0
+    if not os.path.exists(vo_path): return np.zeros((48000, 2), dtype=np.int16), 1.0
     sr, data = wavfile.read(vo_path)
     if data.dtype == np.int16: data = data.astype(np.float32) / 32768.0
-    
-    # Resample to 48k if needed
     if sr != 48000:
         tmp_in, tmp_out = "temp_in.wav", "temp_out.wav"
         wavfile.write(tmp_in, sr, (data * 32767).astype(np.int16))
         subprocess.run(["ffmpeg", "-y", "-i", tmp_in, "-ar", "48000", tmp_out], capture_output=True)
         sr, data = wavfile.read(tmp_out); data = data.astype(np.float32) / 32768.0
         os.remove(tmp_in); os.remove(tmp_out)
-    
     if len(data.shape) == 1: data = np.stack([data, data], axis=1)
-    duration = len(data) / 48000.0
-    return (data * 32767).astype(np.int16), duration
+    return (data * 32767).astype(np.int16), len(data) / 48000.0
 
 def generate_formal_mpeg2(assets_dir, output_file):
     scenes = [
         "01_security_line", "02_boot_struggle", "03_sad_sandwich", "04_delayed_sign", "05_gate_lice",
         "06_tiny_seat", "07_crying_baby", "08_seat_recline", "09_turbulence", "10_lost_suitcase"
     ]
-    
     with open(output_file, "wb") as f:
         pts = 90000
         for i, s_id in enumerate(scenes):
-            vo_path = f"{assets_dir}/voice/vo_{i:03d}.wav"
-            audio, duration = get_audio_data_and_duration(vo_path)
-            
+            audio, duration = get_audio_data_and_duration(f"{assets_dir}/voice/vo_{i:03d}.wav")
             num_frames = int(duration * 25)
             if num_frames == 0: num_frames = 1
-            
             audio_bytes = audio.tobytes()
             bytes_per_frame = len(audio_bytes) // num_frames
-            
-            print(f"Processing {s_id}: {duration:.2f}s ({num_frames} frames)")
-            
             for fr in range(num_frames):
                 frame_pts = pts + int(fr * 90000 / 25)
-                # Video Packet
-                write_pes(f, 0xE0, SEQ_HDR + SEQ_EXT + GOP_HDR + PIC_HDR + PIC_EXT + bytes([0, 0, 1, 1, 8, 0]), frame_pts)
-                
-                # Audio Chunk for this frame
+                write_pes(f, 0xE0, FORMAL_CORE_BLOCK, frame_pts)
                 a_start = fr * bytes_per_frame
                 a_end = a_start + bytes_per_frame if fr < num_frames - 1 else len(audio_bytes)
                 a_chunk = audio_bytes[a_start:a_end]
-                if a_chunk:
-                    write_pes(f, 0xBD, a_chunk, frame_pts, is_lpcm=True)
-            
+                if a_chunk: write_pes(f, 0xBD, a_chunk, frame_pts, is_lpcm=True)
             pts += int(duration * 90000)
-            
-        f.write(SEQ_END)
-    print(f"Saved duration-accurate MPEG-2 to {output_file}")
+        f.write(bytes([0, 0, 1, 183])) # SEQ_END
+    print(f"Saved 1-1 aligned MPEG-2 to {output_file}")
 
 if __name__ == "__main__":
     generate_formal_mpeg2("../aigen/airport/assets_airport", "formal_output.mpg")
